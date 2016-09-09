2f5dc43b986e5332f60870de29efa4f203b08bee
Adrian Burcin <adrian.burcin@yahoo.com>
2016-09-09 20:24:51 +0300

    New template and animation Clean up

--- client/js/main.js
+++ client/js/main.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
   var socket = io();
 
   socket.on('status changed', function(response) {
-    $('.status').empty().text(response.status);
+    $('.hsm__control__status').empty().text(response.status);
+    if(response.status === 'OK') {
+      $('.hsm__control__status').removeClass('hsm__error--background-color');
+      $('.hsm__control__inactive-time').removeClass('hsm__error--background-color');
+    } else {
+      $('.hsm__control__status').addClass('hsm__error--background-color');
+      $('.hsm__control__inactive-time').addClass('hsm__error--background-color');
+    }
+    var image = response.status === 'OK' ? 'assets/running.gif' : 'assets/stopped.gif';
+    $('.hsm__image-status--border').attr('src', image);
 
     var lastCheck = moment(response.lastCheck);
-    $('.last-check').empty().text(lastCheck.format('MM/DD/YYYY hh:mm:ss'));
+    $('.hsm__control__last-check').empty().text(lastCheck.format('MM/DD/YYYY hh:mm:ss'));
 
     var inactiveTime = moment.utc(response.inactiveTime);
-    $('.inactive-time').empty().text(inactiveTime.format('HH:mm:ss'));
+    $('.hsm__control__inactive-time').empty().text(inactiveTime.format('HH:mm:ss'));
   });
 
-  socket.emit('client connected', 'connected bla bla bla');
+  socket.emit('client connected', 'connected');
 });
